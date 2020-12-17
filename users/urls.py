9f3321436bbded42ad6e7a6b1a859cfb081e1666
VanyaKhomenko <48221525+VanyaKhomenko@users.noreply.github.com>
2020-12-17 18:00:31 +0200

    1
    1

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+"""Определяет схемы URL для пользователей"""
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView
+
+from users import views
+
+urlpatterns = [
+    url('login/', LoginView.as_view(template_name='users/login.html'),name="login"),
+    url(r'^logout/$', views.logout_view, name='logout'),
+
+]
