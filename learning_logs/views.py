86ac759a19f04ca8ab237aa9b98721765b08a7c0
VanyaKhomenko <48221525+VanyaKhomenko@users.noreply.github.com>
2020-12-18 12:48:32 +0200

    15
    Added user roots

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,27 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
-
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, Http404
 
 # Create your views here.
 def index(request):
     """Домашняя страница приложения Learning Log"""
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    """Выводит все темы."""
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Выводит одну тему и все ее записи."""
     topic = Topic.objects.get(id=topic_id)
+    # Проверка того, что тема принадлежит текущему пользователю.
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Определяет новую тему."""
     if request.method != 'POST':
@@ -31,11 +39,14 @@ def new_topic(request):
         # Отправлены данные POST; обработать данные.
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Добавляет новую запись по конкретной теме."""
     topic = Topic.objects.get(id=topic_id)
@@ -54,10 +65,13 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Редактирует существующую запись."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
     if request.method != 'POST':
         # Исходный запрос; форма заполняется данными текущей записи.
         form = EntryForm(instance=entry)
--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,6 @@ from users import views
 urlpatterns = [
     url('login/', LoginView.as_view(template_name='users/login.html'),name="login"),
     url(r'^logout/$', views.logout_view, name='logout'),
-
+    # Страница регистрации
+    url(r'^register/$', views.register, name='register'),
 ]
